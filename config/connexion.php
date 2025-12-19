4e22ab89694f1e997afc6816a644b69d7f615c79
Houda Belkheiri <houdabelkheiri385@gmail.com>
2025-12-19 12:36:50 +0100

    Ajout du fichier connexion.php

---
+++ config/connexion.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = "localhost";           
+$dbname = "gestion_patients"; 
+$user = "root";               
+$pass = "";                    
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $user,
+        $pass
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
